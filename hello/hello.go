59b9429c1fc41d4a3f5a823eda43ce19ad2e3dba
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-07-02 21:30:24 +0100

    Created Go project with modules and tests

---
+++ hello/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	log.SetPrefix("Greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Dave", "Dany", "Joe"}
+
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
+}
